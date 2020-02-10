e875918414554c0e04424641fa75d00b3073cdd0
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-02-10 08:18:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,15 +4,19 @@ class Dog
     @breed = breed
   end
   
-  def breed=(breed)
-    @breed = breed
+  def breed=(new_breed)
+    @breed = new_breed
   end
   
-   def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
   
   def name
     @name 
+  end
+  
+  def breed
+    @breed
   end
 end
